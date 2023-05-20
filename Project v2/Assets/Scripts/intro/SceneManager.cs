1ff833cac895b44a1232834b4eb254750725735f
LauraSofiaEnriquezRomero <128187742+LauraSofiaEnriquezRomero@users.noreply.github.com>
2023-05-19 23:17:14 -0500

    inicio intro del juego

--- Project v2/Assets/Scripts/intro/SceneManager.cs
+++ Project v2/Assets/Scripts/intro/SceneManager.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
-using System.Collections.Generic;
 
 
 public class SceneManager : MonoBehaviour
 {
 
-    public void ChangeScene()
+    public void ChangeScene(int sceneIndex)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        Debug.Log("va cambiando");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
     }
 }
+
 
--- Project v2/Assets/Scripts/level_1/DetectarHechizo.cs
+++ Project v2/Assets/Scripts/level_1/DetectarHechizo.cs
@@ -7,14 +7,14 @@ public class DetectarHechizo : MonoBehaviour
 {
     TCPClient refTCPClient;
     Text refTex;
-    public int[] arregloCuadros = new int[5] {0,0,0,0,0};
+    public int[] arregloCuadros = new int[6] {0,0,0,0,0,0};
     int idxHechizo = 0;
 
     // Hechizos de 5
-    public static int[] expelearmus = new int[5] {2,4,3,5,1};
+    public static int[] expelearmus = new int[6] {0,2,4,2,5,1};
 
     // Hechizos de 3
-    public static int[] adacadabra = new int[5] {5,1,2,0,0};
+    public static int[] adacadabra = new int[6] {0,5,1,2,0,0};
 
 
 
@@ -32,46 +32,13 @@ public class DetectarHechizo : MonoBehaviour
         this.refTex.text = "Hechizo:" + this.refTCPClient.cuadro;
         patronHechizos();
     }
-public void patronHechizos() {
+    public void patronHechizos() {
 
         posicionesUnicas.Add(this.refTCPClient.cuadro);
 
         // Estamos ignorando un cuadro ya leido y los cuadro 0
         if (arregloCuadros[idxHechizo] != this.refTCPClient.cuadro && this.refTCPClient.cuadro != 0) {
-<<<<<<< HEAD
-            arregloCuadros[idxHechizo] = this.refTCPClient.cuadro;
-            
-
-            if (idxHechizo > 2){
-                // puede ser un hechizo de 3
-                if (sonHechizosIguales(arregloCuadros, expelearmus)){
-                    // Es un expelearmus
-                    Debug.Log("Es un expelearmus");
-                    idxHechizo = 0;
-                }
-                idxHechizo++;
-            } else if (idxHechizo > 3){
-                // Es un hechizo de 3
-                if (sonHechizosIguales(arregloCuadros, adacadabra)){
-                    // Es un expelearmus
-                    Debug.Log("Es un adacabra");
-                    idxHechizo = 0;
-                }
-            }idxHechizo++;
-        }
-    }
-
-/*    public void patronHechizos() {
-
-        posicionesUnicas.Add(this.refTCPClient.cuadro);
-
-
-        // Estamos ignorando un cuadro ya leido y los cuadro 0
-        if (arregloCuadros[idxHechizo] != this.refTCPClient.cuadro && this.refTCPClient.cuadro != 0) {
-            //arregloCuadros[idxHechizo] = this.refTCPClient.cuadro;
-=======
->>>>>>> 32ea5759081dc39a0226725aa99e400e5ffa7077
-            idxHechizo = idxHechizo + 1;
+            idxHechizo++;
             arregloCuadros[idxHechizo] = this.refTCPClient.cuadro;
             
             if (idxHechizo > 2){
@@ -79,12 +46,17 @@ public void patronHechizos() {
                 //Puede ser un hechizo de 3.
                 if (idxHechizo > 4) {
                     // Es un hechizo de 5 y ya termninamos.
+                    Debug.Log("El arregloCuadros está lleno");
+                    //limpiar el arreglo 
+                    ReiniciarArreglo();
+
                     if (sonHechizosIguales(arregloCuadros, expelearmus)){
                         // Es un expelearmus
                         Debug.Log("Es un expelearmus");
+                        
                     }
                     // idxHechizo = 0;
-                } else {
+                }else {
                     // Es un hechizo de 3
                     if (sonHechizosIguales(arregloCuadros, adacadabra)){
                         // Es un expelearmus
@@ -95,15 +67,18 @@ public void patronHechizos() {
             }
         }
     }
-*/
+    private void ReiniciarArreglo(){
+        idxHechizo = 0;
+        arregloCuadros = new int[6];
+    }
 
-    public bool sonHechizosIguales(int[] firstArray, int[] secondArray)
+    public bool sonHechizosIguales(int[] expelearmus, int[] arregloCuadros)
     {
-        if (firstArray.Length != secondArray.Length)
+        if (arregloCuadros.Length != expelearmus.Length)
             return false;
-        for (int i = 0; i < firstArray.Length; i++)
+        for (int i = 0; i < arregloCuadros.Length; i++)
         {
-            if (firstArray[i] != secondArray[i])
+            if (arregloCuadros[i] != expelearmus[i])
                 return false;
         }
         return true;
